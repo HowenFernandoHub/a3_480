48733efd51274890376e7375ce367dca544315fa
HowenFernandoHub <jr.ha.fernando@gmail.com>
2022-03-23 19:05:39 -0700

    Cleaned and commented tlb files

--- tlb.hpp
+++ tlb.hpp
@@ -20,18 +20,34 @@
 class tlb
 {
     public:
+        // constructor
         tlb(int vpnNumBits, int capacity);
-        std::map<unsigned int, unsigned int> vpn2pfn;   // k: vpn, v: pfn
-        std::deque<unsigned int> recentPages;    // always be size 10
-        int capacity;
-        bool usingTlb();
-        unsigned int vpnMask;
+
+        // cache mapping of vpn 2 pfn
+        std::map<unsigned int /*vpn*/, unsigned int /*pfn*/> vpn2pfn;
+
+        // queue of most recently accessed pages. Used to determine which mapping to remove from cache
+        std::deque<unsigned int> recentPagesQueue;    // always be size 10
+
+        // cache information
+        int capacity;   // capacity of cache
+        unsigned int vpnMask;       // bit mask for masking off cpn
+
+        // setter method
         void setVpnMask(int vpnNumBits);
+
+        // cache methods
+        bool usingTlb();
         bool hasMapping(unsigned int vpn);
         void insertMapping(unsigned int vpn, unsigned int frameNum);
+
+        // queue methods
         void updateQueue(unsigned int recentVpn);
         bool queueContains(unsigned int vpn);
         void eraseVpnFromQueue(unsigned int vpn);
+        
+        
+        
 };
 
 #endif
